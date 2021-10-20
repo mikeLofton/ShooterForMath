ffaf3a6e070ba052825a31a97b39d851d215e8f5
Michael Lofton <86498451+mikeLofton@users.noreply.github.com>
2021-10-20 16:06:56 -0500

    Initialize Raylib

---
+++ MathForGames/Engine.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        private static bool _applicationShouldClose = false;
+        private static int _currentSceneIndex;
+        private Scene[] _scenes = new Scene[0];
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Called to begin the application
+        /// </summary>
+        public void Run()
+        {
+            //Called start for the entire application
+            Start();
+
+            float currentTime = 0;
+            float lastTime = 0;
+            float deltaTime = 0;
+
+            //Loop until the application is told close
+            while (!_applicationShouldClose && !Raylib.WindowShouldClose())
+            {
+                //Get how much time has passed since the application started
+                currentTime = _stopwatch.ElapsedMilliseconds / 1000.0f;
+
+                //Set delta time to be the difference in time from the last time to the current time
+                deltaTime = currentTime - lastTime;
+
+                //Update the application
+                Update(deltaTime);
+                //Draw all items
+                Draw();
+
+                //Set the last time recorded to be the current time
+                lastTime = currentTime;
+            }
+
+            //Call end for the entire application
+            End();
+        }
+
+        /// <summary>
+        /// Called when the application starts
+        /// </summary>
+        private void Start()
+        {
+            _stopwatch.Start();
+
+            //Create a window using Raylib
+            Raylib.InitWindow(800, 450, "Math For Games");
+            Raylib.SetTargetFPS(60);
+
+            Scene scene = new Scene();
+            
+            Player player = new Player('@', 30, 50, 100, Color.DARKPURPLE, "Player");
+            player.CollisionRadius = 20;
+            Enemy enemy1 = new Enemy('A', 5, 5, 1, player, Color.RED, "Enemy1");
+            enemy1.CollisionRadius = 20;
+
+            //UI Section
+            UIText healthText = new UIText(10, 10, Color.BLUE, "Health", 70, 70, 15, "This is a test. \n All the text inside if this box is not important at all.");
+            scene.AddUIElement(healthText);
+
+            scene.AddActor(player);
+            scene.AddActor(enemy1);
+
+            _currentSceneIndex = AddScene(scene);
+
+            _scenes[_currentSceneIndex].Start();
+
+            Console.CursorVisible = false;
+        }
+        
+        /// <summary>
+        /// Called everytime the gane loops
+        /// </summary>
+        private void Update(float deltaTime)
+        {
+            _scenes[_currentSceneIndex].Update(deltaTime);
+            
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Called every time the game loops to update visuals
+        /// </summary>
+        private void Draw()
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.BLACK);
+
+            //Adds all actor icons to buffer
+            _scenes[_currentSceneIndex].Draw();
+            _scenes[_currentSceneIndex].DrawUI();
+
+            Raylib.EndDrawing();
+        }
+
+        /// <summary>
+        /// Called when the application ends
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+            Raylib.CloseWindow();
+        }
+
+        /// <summary>
+        /// Adds a scene to the engine's scene array
+        /// </summary>
+        /// <param name="scene">The scene that will be added to the scene array</param>
+        /// <returns>The index where the new scene is located</returns>
+        public int AddScene(Scene scene)
+        {
+            //Creates a new temporary array
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            //Copy all values from old array into the new array
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+
+            //Set the last index to be the new scene
+            tempArray[_scenes.Length] = scene;
+
+            //Set the old array to be the new array
+            _scenes = tempArray;
+
+            //Return the last index
+            return _scenes.Length - 1;
+        }
+
+        /// <summary>
+        /// Gets the next key in the input stream
+        /// </summary>
+        /// <returns>The key that was pressed</returns>
+        public static ConsoleKey GetNextKey()
+        {
+            //If there is no key being pressed...
+            if (!Console.KeyAvailable)
+                //...return
+                return 0;
+
+            //Return the current key being pressed
+            return Console.ReadKey(true).Key;
+        }
+
+        /// <summary>
+        /// Ends the application
+        /// </summary>
+        public static void CloseApplication()
+        {
+            _applicationShouldClose = true;
+        }
+    }
+}
---
+++ MathForGames/UIText.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class UIText : Actor
+    {
+        public string Text;
+        public int Width;
+        public int Height;
+        public int FontSize;
+        public Font Font;
+
+        /// <summary>
+        /// Sets the starting value for the text box
+        /// </summary>
+        /// <param name="x">The x position of the text box</param>
+        /// <param name="y">The y position of the text box</param>
+        /// <param name="name"></param>
+        /// <param name="color"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="text"></param>
+        public UIText(float x, float y, Color color, string name, int width, int height, int fontSize, string text = "")
+            : base('\0', x, y, color, name)
+        {
+            Text = text;
+            Width = width;
+            Height = height;
+            Font = Raylib.LoadFont("resources/fonts/alagard.png");
+            FontSize = fontSize;
+        }
+
+        public override void Draw()
+        {
+            //Create a new rectangle that will act as the borders of the text box
+            Rectangle textBox = new Rectangle(Position.X, Position.Y, Width, Height);
+            //Draw text box
+            Raylib.DrawTextRec(Font, Text, textBox, FontSize, 1, true, Icon.Color);         
+        }
+    }
+}
