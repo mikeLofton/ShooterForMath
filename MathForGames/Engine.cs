fd574d70cec70bdc6becade660d4b3e792511fa7
mikeLofton <michaellofton32@gmail.com>
2021-10-27 16:42:22 -0500

    Added images folder

--- MathForGames/Engine.cs
+++ MathForGames/Engine.cs
@@ -62,22 +62,22 @@ namespace MathForGames
 
             Scene scene = new Scene();
             
-            Player player = new Player(400, 225, 100, scene, "Player", "player.png");
+            Player player = new Player(400, 225, 100, scene, "Player", "Images/player.png");
             player.SetScale(50, 50);
             AABBCollider playerBoxCollider = new AABBCollider(30, 30, player);
             player.Collider = playerBoxCollider;
 
-            Enemy enemy1 = new Enemy(5, 5, 1, player,"Enemy1", "enemy.png");
+            Enemy enemy1 = new Enemy(25, 40, 1, player,"Enemy1", "Images/enemy.png");
             enemy1.SetScale(50, 50);
             AABBCollider enemy1BoxCollider = new AABBCollider(30, 30, enemy1);
             enemy1.Collider = enemy1BoxCollider;
 
-            Enemy enemy2 = new Enemy(400, 400, 1, player, "Enemy2", "enemy.png");
+            Enemy enemy2 = new Enemy(400, 400, 1, player, "Enemy2", "Images/enemy.png");
             enemy2.SetScale(50, 50);
             AABBCollider enemy2BoxCollider = new AABBCollider(30, 30, enemy2);
             enemy2.Collider = enemy2BoxCollider;
 
-            Enemy enemy3 = new Enemy(5, 400, 1, player, "Enemy3", "enemy.png");
+            Enemy enemy3 = new Enemy(25, 400, 1, player, "Enemy3", "Images/enemy.png");
             enemy3.SetScale(50, 50);
             AABBCollider enemy3BoxCollider = new AABBCollider(30, 30, enemy3);
             enemy3.Collider = enemy3BoxCollider;
